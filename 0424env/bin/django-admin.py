6cc2357ff5493988fa19a52518e3104c2a4c0239
username <you@example.com>
2020-04-24 13:35:12 +0900

    My Django Girls app, first commit

---
+++ 0424env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/katousachi/Desktop/python_lesson/djangogirls/0424env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
